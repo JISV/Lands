f093f3dbb983e6e38b17a24a00102e49f4031cb4
jisv <jisv@github.com>
2018-07-08 20:15:20 -0500

    Initial

---
+++ Lands/App.xaml.cs
@@ -0,0 +1,36 @@
+namespace Lands
+{
+    using System;
+    using Lands.Views;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
