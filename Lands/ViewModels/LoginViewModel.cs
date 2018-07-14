8876c06d5db820b0878b4d62e715408b75e7e415
jisv <josesotovasquez@gmail.com>
2018-07-13 22:18:23 -0500

    5 Commit
    PropertyChanged

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -4,11 +4,29 @@
 namespace Lands.ViewModels
 {
     using System;
+    using System.ComponentModel;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel: INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        //En los atributos pongo las propiedades privadas para los
+        //campos que se tengan que refrescar en los controles
+        //las pongo con el mismo nombre que las publicas pero en minuscula
+        #region Atributes
+            private string email;
+            private string password;
+            private bool isRunning;
+            private bool isEnabled;
+        #endregion
+
+
+
         #region Properties
         public string Email
         {
@@ -18,14 +36,38 @@ namespace Lands.ViewModels
 
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+               if (this.password != value) 
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
 
         public bool IsRemembered
@@ -34,9 +76,28 @@ namespace Lands.ViewModels
             set;
         }
 
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
+        }
+
+
         #endregion
 
-        //Los ciomandos son propiedades especiales y les creamos una region para ellos
+        //Los comandos son propiedades especiales y les creamos una region para ellos
         //y son del tipo interface
         #region Commands
         public ICommand LoginCommand
@@ -48,10 +109,52 @@ namespace Lands.ViewModels
 
 
         }
+
+
+
         //Aqui validamos lo que el usuario escribe para el login
-        private void Login()
+        private async void Login()
         {
-            
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            };
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password.",
+                    "Accept");
+                return;
+            };
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jisv@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Invalid email or password.",
+                    "Accept");
+                //limpio el campo
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Usuario validado",
+                    "Accept");
         }
 
         public ICommand RegisterCommand
@@ -65,6 +168,9 @@ namespace Lands.ViewModels
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            //por defecto los bool son false por lo que para habilitar
+            //los botones tengo que poner IsEnabled true
+            this.isEnabled = true;
         }
         #endregion
     }
