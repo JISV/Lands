2e272b01077f48afa266f45b2a52ed9db9aa8764
jisv <josesotovasquez@gmail.com>
2018-07-13 19:32:34 -0500

    Commit 3
    Controles y MVVM arquitectura

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+
+
+namespace Lands.ViewModels
+{
+using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        //Los ciomandos son propiedades especiales y les creamos una region para ellos
+        //y son del tipo interface
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+
+        public ICommand RegisterCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+    }
+}
