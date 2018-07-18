981daa719abf77cbfe9989bc1ef415b5539c6f06
jisv <josesotovasquez@gmail.com>
2018-07-17 20:12:53 -0500

    Commit 10
    ListView Lands

---
+++ Lands/Models/Response.cs
@@ -0,0 +1,27 @@
+﻿namespace Lands.Models
+{
+    public class Response
+    {
+        //Esta clase me indica si la comunicacion fue exitosa
+        //si viene success true entonces me trae el objeto eesult
+        //si algo fallo me trae el mensage de error
+
+        public bool IsSuccess
+        {
+            get;
+            set;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public object Result
+        {
+            get;
+            set;
+        }
+    }
+}
--- Lands/ViewModels/LandsViewModel.cs
+++ Lands/ViewModels/LandsViewModel.cs
@@ -1,9 +1,102 @@
-﻿namespace Lands.ViewModels
+﻿
+
+namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using Services;
+    using Xamarin.Forms;
+
+    public class LandsViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+
+        #region Attributes
+        private bool isRefreshing;
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands; }
+            set { SetValue(ref this.lands, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        #endregion
+
+        #region Constructors
         public LandsViewModel()
         {
+            this.apiService = new ApiService();
+            this.LoadLands();
+
         }
+
+        #endregion
+
+
+        #region Methods
+        private async void LoadLands()
+        {
+            this.IsRefreshing = true;
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                connection.Message,
+                "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
+            var response = await this.apiService.GetList<Land>(
+                "http://restcountries.eu",
+                "/rest",
+                "/v2/all");
+            if (!response.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                response.Message,
+                "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
+            //Si llega hasta aqui es que tengo la lista
+            //Asi que creo una var lista y la casteo a un objeto tipo lista
+            //luego lo convieto en un obsevablecollection
+
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+        #endregion
     }
 }
