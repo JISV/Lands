d286a64571fbe3a9ad34db7be70d6d0bbcd0de89
jisv <josesotovasquez@gmail.com>
2018-07-19 11:21:53 -0500

    Commit 11
    Search bar

--- Lands/ViewModels/LandsViewModel.cs
+++ Lands/ViewModels/LandsViewModel.cs
@@ -5,6 +5,7 @@ namespace Lands.ViewModels
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Linq;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Models;
@@ -21,6 +22,8 @@ namespace Lands.ViewModels
         #region Attributes
         private bool isRefreshing;
         private ObservableCollection<Land> lands;
+        private string filter;
+        private List<Land> landsList;
         #endregion
 
         #region Properties
@@ -34,6 +37,18 @@ namespace Lands.ViewModels
         {
             get { return this.isRefreshing; }
             set { SetValue(ref this.isRefreshing, value); }
+        }
+
+        public string Filter
+        {
+            get { return this.filter; }
+            set { 
+                SetValue(ref this.filter, value);
+                //pongo esta instruccion aqui para que me vaya 
+                //actualizando la busqueda del searchbar cada vez que 
+                //coloque un caracter
+                this.Search();
+            }
         }
         #endregion
 
@@ -83,9 +98,26 @@ namespace Lands.ViewModels
             //Asi que creo una var lista y la casteo a un objeto tipo lista
             //luego lo convieto en un obsevablecollection
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.landsList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
             this.IsRefreshing = false;
+        }
+
+        private void Search()
+        {
+            if(string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList);
+            }
+            else
+            {
+                //busco por nombre del pais o de la capital
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList.Where(l =>l.Name.ToLower().Contains(this.Filter.ToLower()) ||
+                                         l.Capital.ToLower().Contains(this.Filter.ToLower())));
+            }
+
         }
         #endregion
 
@@ -97,6 +129,17 @@ namespace Lands.ViewModels
                 return new RelayCommand(LoadLands);
             }
         }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+
+
         #endregion
     }
 }
