753f84e2b7ce64076a444cfd33af20f2f2af58ae
Jose Soto <josesotovasquez@gmail.com>
2018-08-16 17:56:18 -0400

    User Model Commit 23

---
+++ Lands.Domain/User.cs
@@ -0,0 +1,61 @@
+﻿namespace Lands.Domain
+{
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "First Name")]//Display es como queremos que aparezca el nombre del campo
+        [Required(ErrorMessage = "The field {0} is requiered.")]//estos mensajes son solodel backend no del app, no son multi-idioma
+        [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "The field {0} is requiered.")]
+        [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        public string LastName { get; set; }
+
+        //Dos usuarios no pueden tener el mismo email por aqui lo ligamos con el backend y por eso creamos el index
+        [Required(ErrorMessage = "The field {0} is requiered.")]
+        [MaxLength(100, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        [Index("User_Email_Index", IsUnique = true)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        [DataType(DataType.PhoneNumber)]
+        public string Telephone { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImagePath))
+                {
+                    return "noimage";
+                }
+
+                return string.Format(
+                    "https://landsdbapi.azurewebsites.net/{0}",
+                    ImagePath.Substring(1));
+            }
+        }
+
+        [Display(Name = "User")]
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+    }
+}
